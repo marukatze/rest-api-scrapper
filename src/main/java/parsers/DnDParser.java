cb6d921796ae53b916f0e29f8bcb1183a7851197
marukatze <kaffeekoenigg@gmail.com>
2025-10-13 15:53:06 +0300

    update DnDParser for DnD-API responses

--- src/main/java/parsers/DnDParser.java
+++ src/main/java/parsers/DnDParser.java
@@ -1,4 +1,23 @@
 package parsers;
 
-public class DnDParser {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.APIParser;
+import core.DataRecord;
+
+import java.time.LocalDateTime;
+
+public class DnDParser implements APIParser {
+    private final String source = "dnd-api";
+
+    @Override
+    public DataRecord parse(String json) throws JsonProcessingException {
+        JsonNode node = new ObjectMapper().readTree(json);
+        return new DataRecord(
+                "dnd-api",
+                node.get("name").asText(),
+                node.get("desc").asText(),
+                LocalDateTime.now());
+    }
 }
