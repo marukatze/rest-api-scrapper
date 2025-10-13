2ed72c4db391e13aeb567015c5f85380deaf8c43
marukatze <kaffeekoenigg@gmail.com>
2025-10-13 18:23:38 +0300

    mover APIParser to utils

--- src/main/java/utils/APIParser.java
+++ src/main/java/utils/APIParser.java
@@ -1,7 +1,8 @@
-package core;
+package utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import core.DataRecord;
 
 public abstract class APIParser {
     private static final ObjectMapper mapper = new ObjectMapper();
