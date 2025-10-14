b31e221d9d87783d3337a913581193779c030626
marukatze <kaffeekoenigg@gmail.com>
2025-10-14 13:47:21 +0300

    mini import fix

--- src/main/java/parsers/DnDParser.java
+++ src/main/java/parsers/DnDParser.java
@@ -2,7 +2,6 @@ package parsers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import utils.APIParser;
 import core.DataRecord;
 
