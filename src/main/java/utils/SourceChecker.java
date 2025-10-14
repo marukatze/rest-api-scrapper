28bcfe8506ec10ca9f7fb8ab11f0f6a400f04778
marukatze <kaffeekoenigg@gmail.com>
2025-10-14 15:11:44 +0300

    add SourceChecker util class

---
+++ src/main/java/utils/SourceChecker.java
@@ -0,0 +1,13 @@
+package utils;
+
+import core.Source;
+import exceptions.InvalidSource;
+
+public class SourceChecker {
+    public static Source checkSource(String sourceName) throws InvalidSource {
+        if (sourceName.contains("achn")) return Source.ACHN;
+        else if (sourceName.contains("dnd")) return Source.DND;
+        else if (sourceName.contains("cat")) return Source.CATS;
+        else throw new InvalidSource("this source isn't supported");
+    }
+}
