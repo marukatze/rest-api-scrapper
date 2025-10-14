b3415e7177040ec7135bcc04f5c0331f814b8bc4
marukatze <kaffeekoenigg@gmail.com>
2025-10-14 21:42:17 +0300

    mini fix

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -16,7 +16,7 @@ public class Main {
     private static FileFormat format;
 
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         try {
             parseArgs(args);
         } catch (InvalidFileFormat | InvalidSource | NumberFormatException e) {
